757ce841806d60f2135fb59044930659797fdcbd
Jeri V.L. <jerivl@pm.me>
2026-04-15 18:05:44 -0700

    nix doesn't like runtime conf changes

--- src/addon/__init__.py
+++ src/addon/__init__.py
@@ -2,7 +2,6 @@ from .ankiaddonconfig import ConfigManager
 from .migrate import maybe_migrate_config
 
 conf = ConfigManager()
-maybe_migrate_config(conf)
 
 from . import colors
 from . import menu
