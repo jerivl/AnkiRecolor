31f76a0a04fe0a247e00acf7dd364c54bc3cc7c3
Jeri V.L. <jerivl@pm.me>
2026-04-15 18:39:11 -0700

    just in case

--- src/addon/__init__.py
+++ src/addon/__init__.py
@@ -1,8 +1,10 @@
-from .ankiaddonconfig import ConfigManager
-from .migrate import maybe_migrate_config
+from .ankiaddonconfig import ConfigManager 
+from . import colors 
+from colors import recolor_python, recolor_web 
 
-conf = ConfigManager()
+conf = ConfigManager() 
+colors.recolor_python()
+colors.recolor_web()
 
-from . import colors
 from . import menu
 from . import config
